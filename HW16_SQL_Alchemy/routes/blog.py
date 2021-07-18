31febd4baced1692d773ef634f6b01f47fe4082d
Oleksandr Korobchuk <korobchuk.aleksandr@gmail.com>
2021-07-18 21:07:58 +0300

    added hw_17_Flask_Practice (Registration form)

--- HW16_SQL_Alchemy/routes/blog.py
+++ HW16_SQL_Alchemy/routes/blog.py
@@ -1,142 +1,101 @@
+import os
+import datetime
 from app import app, api, db
-from flask import render_template, request, Response
+from flask import render_template, request, Response, redirect, session
 from config import Config, articles
 from flask_restful import Resource, Api
 from models.models import Article, User
+from helpers.additional_functions import check_password
 
 
 @app.route('/', methods=["GET"])
 def homepage():
     articles =Article.query.all()
+    session['hello'] = 'hello world'
     return render_template('blog/index.html', config=Config, articles=articles)
+
+@app.route('/sign-up', methods=["GET"])
+def sign_up():
+    if session.get('user', None):
+        return redirect('/')
+    return render_template('blog/shared/signup.html')
+
+
+@app.route('/user-register', methods=['POST'])
+def user_store():
+    data = request.form
+    user = User(
+        username=data.get('username'),
+        email=data.get('email'),
+        password=data.get('password'),
+        bio=data.get('description'),
+        created=datetime.datetime.now(),
+        admin=0,
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    session['user'] = user.serialize
+    return redirect("/")
+
+
+@app.route ('/logout', methods = ['GET'])
+def logout():
+    session.pop('user')
+    return redirect('/')
+
+
+
+@app.route('/sign-in', methods=["GET"])
+def sign_in():
+    if session.get('user', False):
+        return redirect('/')
+    return render_template('blog/shared/signin.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    user = User.query.filter_by(email=request.form.get('username')).first()
+    if user:
+        if check_password(user.password, request.form.get('password')):
+            session['user'] = user.serialize
+    else:
+        user = User.query.filter_by(username=request.form.get('username')).first()
+        if user:
+            if check_password(user.password, request.form.get('password')):
+                session['user'] = user.serialize
+
+    return redirect('/')
 
 @app.route('/article/<string:slug>')
 def article_details(slug):
     article = Article.query.filter_by(slug=slug).first()
-    return render_template ('blog/details.html', article=article)
+    return render_template('blog/details.html', article=article)
 
 
-class MenuItem(Resource):
-    def get(self):
-        return {
-            'success': True,
-            'items': Config.MENU_ITEMS
-        }
-
-class Articles(Resource):
-    def post(self):
-        data = request.json
-        article = Article(
-            author_id=data.get('author_id'),
-            title=data.get('title'),
-            slug=data.get('slug'),
-            description=data.get('description'),
-            short_description=data.get('short_description'),
-            img=data.get('img')
-        )
-        db.session.add(article)
-        db.session.commit()
-        return article.serialize
-
-    def get(self):
-        articles = Article.query
-        if request.args.get('title'):
-            articles = articles.filter_by(title=request.args.get('title'))
-
-        # articles = articles.filter(Article.title.startwith('A'))
-
-        if request.args.get("sort_by"):
-            articles = articles.order_by(request.args.get("sort_by")).all()
+@app.route('/article/create')
+def article_create():
+    return render_template('/blog/article_create.html')
 
 
-        articles = articles.all()
-        serialized_articles = []
-        for article in articles:
-            serialized_articles.append(article.serialize)
-
-        return serialized_articles
-
-class ArticlesEntity(Resource):
-    def get(self, id):
-        article = Article.query.get(id)
-        if article == None:
-            return Response(status=404)
-        return article.serialize
+@app.route('/article/store', methods=["POST"])
+def article_store():
+    data = request.form
+    img = request.files['img']
+    if img:
+        img.save(os.path.join(Config.UPLOAD_PATH, img.filename))
+        path = "/" + Config.UPLOAD_PATH +img.filename
 
 
-
-class Users(Resource):
-    def get(self):
-        user = User.query.get(1)
-        serialized_articles = []
-        for article in user.articles:
-            print(article)
-            serialized_articles.append(article.serialize)
-        return serialized_articles
-
-
-class UsersRead(Resource):
-    def get(self, id):
-        user = User.query.get(id)
-        if user == None:
-            return Response(status=404)
-        serialized_articles = []
-        for article in user.articles:
-            print(article)
-            serialized_articles.append(article.serialize)
-        serialized_user = [user.serialize, serialized_articles]
-        return serialized_user
-
-
-
-class UsersUpdate(Resource):
-    def post(self, id):
-        user = User.query.get(id)
-        data = request.json
-        user.username = data.get('username')
-        user.password = data.get('password')
-        user.email = data.get('email'),
-        user.created = data.get('created'),
-        user.bio = data.get('bio'),
-        user.admin = data.get('admin')
-
-
-        db.session.add(user)
-        db.session.commit()
-        return user.serialize
-
-
-
-class UsersCreate(Resource):
-    def post(self):
-        data = request.json
-        user = User(
-            username=data.get('username'),
-            password=data.get('password'),
-            email=data.get('email'),
-            created=data.get('created'),
-            bio=data.get('bio'),
-            admin=data.get('admin')
-        )
-        db.session.add(user)
-        db.session.commit()
-        return user.serialize
-
-
-
-class FooterItem(Resource):
-    def get(self):
-        return {
-            'success': True,
-            'items': Config.FOOTER_LINKS
-        }
-
-
-api.add_resource(MenuItem, '/menu-items')
-api.add_resource(Articles, '/articles')
-api.add_resource(ArticlesEntity, '/articles/<int:id>')
-api.add_resource(Users, '/users')
-api.add_resource(UsersRead, '/users/read/<int:id>')
-api.add_resource(UsersUpdate, '/users/update/<int:id>')
-api.add_resource(UsersCreate, '/users/create')
-api.add_resource(FooterItem, '/footer-items')
+    article = Article(
+        author_id=1,
+        title=data.get('title'),
+        slug=data.get('slug'),
+        description=data.get('description'),
+        short_description=data.get('short_description'),
+        img=path
+    )
+    db.session.add(article)
+    db.session.commit()
+    return redirect('/')
